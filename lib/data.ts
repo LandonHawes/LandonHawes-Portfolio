977830e6b97c247d7f199bb5dbd64ef7a5b57d33
LandWigg <94641887+LandWigg@users.noreply.github.com>
2023-12-15 13:05:59 -0600

    fixed import

--- lib/data.ts
+++ lib/data.ts
@@ -2,10 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
-import PreSnapImg from "@/public/presnap2.jpg";
+import PreSnapImg from "@/public/Presnap2.jpg";
 import ExtensionImg from "@/public/extension.jpg";
 import FoodrightImg from "@/public/foodright.jpg";
 
